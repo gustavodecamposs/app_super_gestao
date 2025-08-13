0180992332f46f6e39cff97c3c081b7c0b6d3c14
gustavodecamposs <gustavoo.dcsilva@gmail.com>
2025-08-13 11:01:40 -0300

    Atualizações routes, controller, view

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){ return'Login';})->name('site.login');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){ return'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){ return'Produtos';})->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe, <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial.';
+});
+
 // nome, categoria, assunto, mensagem
 
-Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}', function(
-    string $nome = 'Desconhecido', 
-    string $categoria = 'Informação', 
-    string $assunto = 'Contato', 
-    string $mensagem = 'Mensagem não informada') 
-    {
-    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
-});
+
 
 /* verbo http
 
