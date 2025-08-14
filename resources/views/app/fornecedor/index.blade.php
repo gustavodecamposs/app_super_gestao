32083f5b93a23e4420f58b55c0564bfb831fbd5a
gustavodecamposs <gustavoo.dcsilva@gmail.com>
2025-08-14 11:06:24 -0300

    .

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,21 @@
+<h3>Fornecedor</h3>
+
+
+{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
+
+@php 
+    /*
+    if (isset($variavel)){} //retornar true se a variavel estar definida
+    */
+@endphp
+
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores [0] ['nome'] }} 
+    <br>
+    Status: {{ $fornecedores [0] ['status'] }}
+    <br>
+    @isset($fornecedores[0] ['cnpj'])
+        CNPJ: {{ $fornecedores [0] ['cnpj'] }}
+    @endisset
+    <br>
+@endisset
